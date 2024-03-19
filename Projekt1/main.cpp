fb8cdf425134db3b9e2ad7fd20508f60fab1bac7
Freddy-Ms <maciejskrzypa2705@gmail.com>
2024-03-19 23:43:27 +0100

    Lista dwukierunkowa

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include"DynamicT.cpp"
+#include"List.cpp"
 using namespace std;
 
 int main()
 {
-    DynamicT d(10);
-    d.addfirst(5);
-    d.addfirst(4);
-    d.print();
-    d.addlast(6);
-    d.addlast(7);
-    d.print();
-    d.addatindex(8,2);
-    d.addatindex(9,2);
-    d.print();
-    d.removemiddle(2);
-    d.print();
-    d.removelast();
-    d.print();
-    d.removefirst();
-    d.print();
+    bidirectional_list l;
+    l.addfirst(5);
+    l.addfirst(4);
+    l.print();
+    l.addlast(6);
+    l.addlast(7);
+    l.print();
+    l.addatindex(8,2);
+    l.addatindex(9,2);
+    l.print();
+    l.removemiddle(2);
+    l.print();
+    l.removelast();
+    l.print();
+    l.removefirst();
+    l.print();
+
 }
